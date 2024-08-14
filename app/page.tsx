e005ba375566e9f7a0b23b62475c986826ccb5e7
ghenoo <lucasgheno600@gmail.com>
2024-08-14 11:33:56 -0300

    fix bugs prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-import { Button } from './_components/ui/button';
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
- return <Button>Click me</Button>
+  return (
+    <div>
+      {/* <Header /> */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Usuário</h2>
+        <p>Quarta-feira, 14 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Pesquisar" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores"
+            src="/banner.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
